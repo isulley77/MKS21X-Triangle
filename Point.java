d48defffb57710006c0e829106381606e8de6456
isulley77 <isulley@stuy.edu>
2018-10-10 22:48:47 -0400

    Code Added

--- Point.java
+++ Point.java
@@ -1,1 +1,53 @@
+
+public class Point{
+    
+    private double x;
+    private double y;
+    
+    public Point(double x, double y){
+    
+        x=X;
+        y=Y;
+    
+    }
+    
+    public Point(Point p){
+    
+        x = p.x;
+        y = p.y;
+    }
+    
+    public double getX(){
+    
+        return x;
+        
+    }
+    
+    public double getY(){
+    
+        return y;
+        
+    }
+    
+    public double distanceTo(Point p){
+    
+        return Math.sqrt(Math.pow(this.x-p.x,2) + Math.pow(this.y-p.y, 2));
+    }
+    
+    public static double distance(Point p1, Point p2){
+    
+        return Math.sqrt(Math.pow(p1.x-p2.x,2) + Math.pow(p1.y-p2.y,2));
+        
+    }
+    
+    public String toString(){
+    
+        return("X:" + x + "\n" + "Y:" + y);
+        
+    }
+}
+
+
+
+
  
--- Triangle.java
+++ Triangle.java
@@ -1,1 +1,28 @@
  
+public class Triangle {
+
+    private Point p1;
+    private Point p2;
+    private Point p3;
+    
+    public Triangle(Point p1, Point p2, Point p3){
+    
+        this.p1 = p1;
+        this.p2 = p2;
+        this.p3 = p3;
+            
+    }
+    
+    public Triangle(double x1, double y1, double x2, double y2, double x3, double y3){
+    
+        p1 = new p1 (x1, y1);
+        p2 = new p2 (x2, y2);
+        p3 = new p3 (x3, y3);
+        
+    }
+    
+    public double getPerimeter() {
+     
+        return p1.distanceTo(p2) + p2.distanceTo(p3) + p3.distanceTo(p1);
+        
+    }
